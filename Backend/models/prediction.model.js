f1e3d504974be3d5e51284bda97c7b61a1e235cc
satvik297 <satvikkakkar5@gmail.com>
2024-11-16 14:40:39 +0530

    backend made

---
+++ Backend/models/prediction.model.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const PredictionSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // Reference to the user
+  test_type: { 
+    type: String, 
+    enum: ['Stress', 'Anxiety', 'Depression'], 
+    required: true 
+  }, // Indicates the type of test
+  result: { 
+    type: String, 
+    required: true 
+  }, // Result level, e.g., "None", "Mild", "Moderate", "Severe"
+  created_at: { type: Date, default: Date.now }, // Timestamp for when the prediction was made
+});
+
+const Prediction = mongoose.model('Prediction', PredictionSchema);
+
+export default Prediction;
